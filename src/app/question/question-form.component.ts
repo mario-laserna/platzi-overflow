2193627fac399c8eaa78118e18da338ba161b874
Mario Laserna <laserna.mario@gmail.com>
2018-11-09 16:40:17 -0500

    se acutalizan fuentes segun avances

--- src/app/question/question-form.component.ts
+++ src/app/question/question-form.component.ts
@@ -1,12 +1,27 @@
 import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {Question} from './question.model';
+import icons from './icons';
 
 @Component({
   selector: 'app-question-form',
   templateUrl: './question-form.component.html'
 })
 export class QuestionFormComponent {
+  icons: Object[] = icons;
+
+  getIconVersion(icon: any) {
+    let version;
+
+    if (icon.versions.font.includes('plain-wordmark')) {
+      version = 'plain-wordmark';
+    } else {
+      version = icon.version.font[0];
+    }
+
+    return version;
+  }
+
   onSubmit(form: NgForm) {
     const q = new Question(
       form.value.titulo,
