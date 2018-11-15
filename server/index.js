805db7f01753c003466251f81e3b75d0dd766ae3
Mario Laserna <laserna.mario@gmail.com>
2018-11-15 13:13:43 -0500

    se agrega paquete debug para mensajes en consola y se configura package para incluirlo en start server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 import http from 'http'
+import Debug from 'debug'
 
 const PORT = 3000
+const debug = new Debug('platzi-overflow:root')
 
 const app = http.createServer((req, res) => {
+  debug('Petición recibida')
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.write('Hola desde PlatziOverflow')
   res.end()
 })
 
 app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`)
+  //console.log(`Server running at port ${PORT}`)
+  debug(`Server running at port ${PORT}`)
 })
