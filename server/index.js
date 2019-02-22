4793753819125812bce62b1cfb16cf929bd46649
Mario <mlaserna@consensussa.com>
2019-02-22 12:08:53 -0500

    se ajusta package para usar concurrently y se instalan paquetes de expres

--- server/index.js
+++ server/index.js
@@ -1,15 +1,9 @@
 import http from 'http'
 import Debug from 'debug'
+import app from './app'
 
 const PORT = 3000
 const debug = new Debug('platzi-overflow:root')
-
-const app = http.createServer((req, res) => {
-  debug('Petición recibida')
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.write('Hola desde PlatziOverflow')
-  res.end()
-})
 
 app.listen(PORT, () => {
   //console.log(`Server running at port ${PORT}`)
